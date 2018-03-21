d815c428614901d147803fdccdd4b9e334bd4448
Arkiherttua <Arkiherttua@users.noreply.github.com>
2018-03-21 21:38:01 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,8 +1,8 @@
 document.write('<div>\
- <nav>
-  <a href="index.html">Koti</a></li>
-  <a href="linkit.html">Linkkilista</a></li>
-  <a href="https://scratch.mit.edu">Scratch</a></li>
-</nav>
+ <nav>\
+  <a href="index.html">Koti</a></li>\
+  <a href="linkit.html">Linkkilista</a></li>\
+  <a href="https://scratch.mit.edu">Scratch</a></li>\
+</nav>\
     </div>\'
 );
